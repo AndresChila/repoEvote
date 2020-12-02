842304e92ba113ea03872326d9cb5dba1f592891
Andres Chila <andres.ch.quiroga@hotmail.com>
2020-12-02 08:56:20 -0500

    ip

--- app/Http/Controllers/Conexion.php
+++ app/Http/Controllers/Conexion.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 class Conexion 
 {
     //IP servicio de autenticacion
-    protected $IP_SERVER = '3.91.59.186';
+    protected $IP_SERVER = '54.160.244.55';
 
     public function conectar(){
         return $this->IP_SERVER;
